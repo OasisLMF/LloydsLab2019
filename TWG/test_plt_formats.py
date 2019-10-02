62efe17a108574dfd6dcf2013b24da2f581accac
Mark Pinkerton <mark.pinkerton@oasislmf.org>
2019-10-02 16:11:40 +0100

    Added

--- TWG/test_plt_formats.py
+++ TWG/test_plt_formats.py
@@ -17,6 +17,8 @@ import gc
 import dask.dataframe as dd
 from pathlib import Path
 from scipy.stats import beta, poisson
+import sqlalchemy
+import sqlalchemy_utils
 
 #
 # Utility to benchmark size and query perfomance of
@@ -28,7 +30,7 @@ def query_aal_by_summary(summary_df, plt_df, num_periods, num_samples):
     summary_df.set_index('summary_id')
     plt_df.set_index('summary_id')
     plt_df = plt_df.merge(summary_df, on='summary_id')
-    plt_df.groupby('value').apply(
+    aal_df = plt_df.groupby('value').apply(
         lambda x: x['loss'].sum()/num_periods * num_samples).compute()
 
 
@@ -52,6 +54,40 @@ def query_aep_by_summary_csv(summary_file, plt_file, num_periods):
     summary_df = dd.read_csv(summary_file)
     plt_df = dd.read_csv(plt_file)
     query_aep_by_summary(summary_df, plt_df, num_periods)
+
+
+def query_aal_by_summary_sql(engine, num_periods, num_samples):
+    rs = engine.execute(""" 
+        SELECT    
+         summary_id, 
+         SUM(loss) / {}
+        FROM
+         plt
+        GROUP BY
+         summary_id
+    """.format(num_periods * num_samples))
+
+
+def query_aep_by_summary_sql(engine, num_periods):
+    rs = engine.execute(""" 
+        SELECT
+         summary_id,
+         percentile_cont({}) WITHIN GROUP (ORDER BY loss ASC)
+        FROM
+        (
+         SELECT 
+          summary_id,
+          period,
+          SUM(loss) AS loss
+         FROM 
+          plt 
+         GROUP BY
+          summary_id,
+          period
+        ) AS aplt
+        GROUP BY
+         summary_id
+    """.format(min(200.0 / num_periods, 1.0)))
 
 
 def query_aal_by_summary_gz_csv(summary_file, plt_file, num_periods, num_samples):
@@ -82,16 +118,20 @@ def query_aep_by_summary_parquet(summary_file, plt_file, num_periods):
 @click.argument('num_periods', required=True, type=int)
 @click.argument('event_rate', required=True, type=int)
 @click.argument('num_samples', required=True, type=int)
-@click.option('--num_summary_sets', type=int, default=1)
+@click.argument('conn_string', required=True, type=str)
+@click.argument('output_file', required=True, type=str)
 @click.option('--num_summaries_per_summary_set', type=int, default=10)
 @click.option('--prob_of_loss', type=float, default=0.1)
 @click.option('--loss_alpha', type=float, default=1.0)
 @click.option('--loss_beta', type=float, default=10.0)
 @click.option('--loss_max', type=float, default=100000.0)
+@click.option('--do_header', is_flag=True)
 def main(
         num_periods, event_rate,
-        num_samples, num_summary_sets, num_summaries_per_summary_set,
-        prob_of_loss, loss_alpha, loss_beta, loss_max):
+        num_samples, conn_string, output_file,
+        num_summaries_per_summary_set,
+        prob_of_loss, loss_alpha, loss_beta, loss_max,
+        do_header):
 
     with tempfile.TemporaryDirectory() as temp_dir:
 
@@ -123,6 +163,16 @@ def main(
             plt_parquet_file
         )
 
+        # Insert into database
+        if sqlalchemy_utils.database_exists(conn_string):
+            sqlalchemy_utils.drop_database(conn_string)
+        sqlalchemy_utils.create_database(conn_string)
+        engine = sqlalchemy.create_engine(conn_string)
+        plt_df = pd.read_csv(plt_csv_file)
+        summary_info_df = pd.read_csv(summary_file)        
+        plt_df = plt_df.merge(summary_info_df, on='summary_id')
+        plt_df.to_sql('plt', engine)
+
         gc.collect()
 
         start = time.time()
@@ -137,6 +187,21 @@ def main(
         query_aep_by_summary_csv(summary_file, plt_csv_file, num_periods)
         end = time.time()
         aep_csv_time = end - start
+
+        gc.collect()
+
+        start = time.time()
+        query_aal_by_summary_sql(
+            engine, num_periods, num_samples)
+        end = time.time()
+        aal_sql_time = end - start
+
+        gc.collect()
+
+        start = time.time()
+        query_aep_by_summary_sql(engine, num_periods)
+        end = time.time()
+        aep_sql_time = end - start
 
         gc.collect()
 
@@ -172,20 +237,20 @@ def main(
 
         gc.collect()
 
-        print("CSV size: {}".format(utils.get_readable_filezize(plt_csv_file)))
-        print("CSV AAL query time: {}s".format(round(aal_csv_time, 1)))
-        print("CSV AEP query time: {}s".format(round(aep_csv_time, 1)))
-
-        print("CSV gzip size: {}".format(
-            utils.get_readable_filezize(plt_csv_gz_file)))
-        print("CSV gzip AAL query time: {}s".format(round(aal_gz_csv_time, 1)))
-        print("CSV gzip AEP query time: {}s".format(round(aep_gz_csv_time, 1)))
-
-        print("Parquet size: {}".format(
-            utils.get_readable_filezize(plt_parquet_file)))
-        print("Parquet AAL query time: {}s".format(round(aal_parquet_time, 1)))
-        print("Parquet AEP query time: {}s".format(round(aep_parquet_time, 1)))
-
+        mode = 'w'
+        if do_header:
+            mode = 'a'
+        with open(output_file, mode) as f:
+            if do_header:
+                f.writelines("num_periods,event_rate,num_samples,csv_size,csv_gz_size,parquet_size,csv_aal_time,csv_aep_time,csv_gz_aal_time,csv_gz_aep_time,parquet_aal_time,parquet_aep_time,sql_aal_time,sql_aep_time\n")
+            
+            f.writelines("{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n".format(
+                num_periods, event_rate, num_samples,
+                os.path.getsize(plt_csv_file), os.path.getsize(plt_csv_gz_file), os.path.getsize(plt_parquet_file),
+                round(aal_csv_time, 2), round(aep_csv_time, 2),
+                round(aal_gz_csv_time, 2), round(aep_gz_csv_time, 2),
+                round(aal_parquet_time, 2), round(aep_parquet_time, 2),
+                round(aal_sql_time, 2), round(aep_sql_time, 2)))
 
 if __name__ == "__main__":
     main()
